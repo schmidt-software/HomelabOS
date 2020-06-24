68b321e18c32807ab4f79738669a7c3c1fb0ba49
Evan McCarthy <evan@mccarthy.mn>
2020-06-24 18:54:33 +0000

    use official repo

--- install_homelabos.sh
+++ install_homelabos.sh
@@ -27,8 +27,17 @@ hlos_install() {
 
     printf "\x1B[01;93m========== Install docker ==========\n\x1B[0m"
     # This will get replaced with docker repo with ansible.
-    sudo apt-get install docker.io
-    
+    sudo apt-get remove -y docker docker-engine docker.io containerd runc
+    sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt-get update
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+    printf "\x1B[01;93m========== Install docker-compose ==========\n\x1B[0m"
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+
     printf "\x1B[01;93m========== Install make ==========\n\x1B[0m"
     sudo apt-get install -y make
     
