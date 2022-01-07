1484bd880771bbeb1205750e994860d0fb5d4c77
Timothy Gilbert <timothy.lee.gilbert@gmail.com>
2022-01-06 22:55:58 -0500

    reminder to log out and back in

--- docker_setup.sh
+++ docker_setup.sh
@@ -2,12 +2,12 @@
 
 # If the user is root, end the script.
 if [ "$(id -u)" == "0" ]; then
-    exit 1
+    exit 0
 fi
 
 # If user is not root, check if the user has a group named "docker".
 if [ $(id -Gn | grep -c "docker") -eq 0 ]; then
-    echo "You need to be in the docker group to run this script."
+    printf "\033[92m========== This account is NOT in the docker group ==========\033[0m\n"
     # Ask the user if they want to add the user to the docker group.
     read -p "Do you want to add yourself to the docker group? [y/n] " -n 1 -r
     echo
@@ -15,4 +15,7 @@ if [ $(id -Gn | grep -c "docker") -eq 0 ]; then
         # Add the user to the docker group.
         sudo usermod -aG docker $USER
     fi
+    # Tell the user to log out and log back in for changes to take effect.
+    printf "\033[92m========== You must log out and back in for changes to take effect ==========\033[0m\n"
+    exit 1
 fi
