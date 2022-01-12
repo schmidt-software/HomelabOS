a839dbe19e8b359a582bd45282b8a1246660fa5f
Timothy Gilbert <timothy.lee.gilbert@gmail.com>
2022-01-11 20:43:56 -0500

    fix: check if root first

--- docker_setup.sh
+++ docker_setup.sh
@@ -5,18 +5,20 @@ if [ "$(id -u)" == "0" ]; then
     exit 0
 fi
 
-# If user is not root, check if the user has a group named "docker".
-if [ $(id -Gn | grep -c "docker") -eq 0 ]; then
-    printf "\033[92m========== This account is NOT in the docker group ==========\033[0m\n"
-    # Ask the user if they want to add the user to the docker group.
-    read -p "Do you want to add yourself to the docker group? [y/n] " -n 1 -r
-    echo
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        # Add the user to the docker group.
-        sudo usermod -aG docker $USER
+# If user is not root, check if the user has a group named "docker". Check if user is root first.
+if [ "$(id -u)" != "0" ]; then
+    if [ $(id -Gn | grep -c "docker") -eq 0 ]; then
+        printf "\033[92m========== This account is NOT in the docker group ==========\033[0m\n"
+        # Ask the user if they want to add the user to the docker group.
+        read -p "Do you want to add yourself to the docker group? [y/n] " -n 1 -r
+        echo
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+            # Add the user to the docker group.
+            sudo usermod -aG docker $USER
 
-        # Tell the user to log out and log back in for changes to take effect.
-        printf "\033[92m========== You must log out and back in for changes to take effect ==========\033[0m\n"
-        exit 1
+            # Tell the user to log out and log back in for changes to take effect.
+            printf "\033[92m========== You must log out and back in for changes to take effect ==========\033[0m\n"
+            exit 1
+        fi
     fi
 fi
